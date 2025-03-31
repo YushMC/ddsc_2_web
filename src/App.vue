3329ee62e10a238209864ee710fc8672b50c7e0d
YushMC <mujushaxb10@gmail.com>
2025-03-31 16:34:53 -0600

    add alert inicio

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Swal from "sweetalert2";
 import { ref, onMounted, onBeforeMount } from "vue";
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
@@ -29,6 +30,24 @@ onMounted(async () => {
     const storedColor = defaultColor.value;
     selectedColor.value = storedColor;
     setRootColor(storedColor); // Aplica el color al :root
+  }
+
+  if (!localStorage.getItem("info")) {
+    await Swal.fire({
+      title: "¡Hola, bienvenidos a DDSC 2!",
+      text: "Esta es una beta pública: el diseño y funcionalidades pueden cambiar en cualquier momento. Nos gustaría saber sus retro-alimentaciones por medio de nuestro servidor de discord (Team DDSC Web)!.",
+      icon: "success",
+      showCancelButton: true,
+      confirmButtonText: "Entrar",
+      allowOutsideClick: false, // Evita el cierre al hacer clic fuera
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.setItem("info", "true");
+      } else {
+        window.open("https://www.dokidokispanish.club", "_self");
+      }
+    });
   }
 });
 </script>
